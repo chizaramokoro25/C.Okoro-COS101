49e1a737c68703a67c9c2b28c67e0e543fc591b6
chizaramokoro25 <chizaram.okoro@pau.edu.ng>
2025-10-06 20:02:26 +0100

    did first four practices for week 2

---
+++ week-2/practice_1.rs
@@ -0,0 +1,10 @@
+fn main() {
+	println!("Welcome to COS 101");
+	println!("The course learning outcome is as follows:"); 
+	println!("1. Distinguish between computer and computer programming,/n
+		      2. Develop some techniques in Computer Science,/n
+		      3. Understand the different areas of study in Computer Science,/n
+		      4.Conversant with applications of Computer Science,/n
+		      5.Navigate the career prospects in Computer Science,/n
+		      6.Conversant with computer programming concepts");
+}
