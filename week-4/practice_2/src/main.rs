4d9f1e14aadc95baeb9bc936724fc5345e19e947
chizaramokoro25 <chizaram.okoro@pau.edu.ng>
2025-10-20 14:40:27 +0100

    practice 2 of week 4

--- week-4/practice_2/src/main.rs
+++ week-4/practice_2/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+// rust program to calculate the area of a triangle given three sides
+use std::io;
+
+fn main() 
+{
+    let mut input1 = String::new();
+    let mut input2 = String::new();
+    let mut input3 = String::new();
+
+    println!("Enter your first edge of triangle: ");
+    io::stdin().read_line(&mut input1).expect("not a valid string");
+    let a:f32 = input1.trim().parse().expect("not a valid number");
+
+    println!("Enter second edge of triangle: ");
+    io::stdin().read_line(&mut input2).expect("not a valid string");
+    let b:f32 = input2.trim().parse().expect("not a valid number");
+
+    println!("Enter third edge of triangle: ");
+    io::stdin().read_line(&mut input3).expect("not a valid string");
+    let c:f32 = input3.trim().parse().expect("not a valid number");
+
+    let s:f32 = (a + b + c) / 2.0;
+    let mut area:f32 = s * (s - a) * (s - b) * (s - c);
+    area = area.sqrt();
+
+    println!("Area of a triangle: {}", area);
 }
