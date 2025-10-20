9b4b67f13621145d37ffd5666b6db6e81763bb1d
chizaramokoro25 <chizaram.okoro@pau.edu.ng>
2025-10-20 18:58:10 +0100

    practice 8b of week 4

---
+++ week-4/practice_8b/target/debug/deps/practice_8b.d
@@ -0,0 +1,5 @@
+C:\Users\User\OneDrive\Documents\C.Okoro-COS101\week-4\practice_8b\target\debug\deps\practice_8b.d: src\main.rs
+
+C:\Users\User\OneDrive\Documents\C.Okoro-COS101\week-4\practice_8b\target\debug\deps\practice_8b.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice_8b/target/debug/practice_8b.d
@@ -0,0 +1,1 @@
+C:\Users\User\OneDrive\Documents\C.Okoro-COS101\week-4\practice_8b\target\debug\practice_8b.exe: C:\Users\User\OneDrive\Documents\C.Okoro-COS101\week-4\practice_8b\src\main.rs
