f47dba4426e5fa35ec336a2c385d7048754674aa
chizaramokoro25 <chizaram.okoro@pau.edu.ng>
2025-10-20 18:50:23 +0100

    practice 8a of week 4

---
+++ week-4/practice_8a/target/debug/deps/practice_8a.d
@@ -0,0 +1,5 @@
+C:\Users\User\OneDrive\Documents\C.Okoro-COS101\week-4\practice_8a\target\debug\deps\practice_8a.d: src\main.rs
+
+C:\Users\User\OneDrive\Documents\C.Okoro-COS101\week-4\practice_8a\target\debug\deps\practice_8a.exe: src\main.rs
+
+src\main.rs:
---
+++ week-4/practice_8a/target/debug/practice_8a.d
@@ -0,0 +1,1 @@
+C:\Users\User\OneDrive\Documents\C.Okoro-COS101\week-4\practice_8a\target\debug\practice_8a.exe: C:\Users\User\OneDrive\Documents\C.Okoro-COS101\week-4\practice_8a\src\main.rs
