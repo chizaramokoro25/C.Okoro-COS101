53498088031b127f1b956d9fe5e3eedf317acc96
chizaramokoro25 <chizaram.okoro@pau.edu.ng>
2025-12-01 11:21:20 +0100

    practice 2 0f week 10

---
+++ week-10/practice_2/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+	let v = vec![10, 20, 30];
+	//vector v owns the object in heap
+
+	let v2 = v; //moves ownership to v 
+	display(v2);
+	//v2 is moved to display and v2 is invalidated
+
+}
+fn display(v:Vec<i32>){
+	println!("inside display {:?}", v);
+}
